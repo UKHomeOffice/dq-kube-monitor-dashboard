73f5e8bd41f01fe1b934b00acd358b45e8391364
Haitham Abou Houly <haithamabouhouly@hodqadms-MacBook-Pro.local>
2020-10-26 20:34:23 +0000

    add  URLS  as  env variables

--- app/monitor/scripts/collect_avail_metrics.py
+++ app/monitor/scripts/collect_avail_metrics.py
@@ -17,12 +17,12 @@ log.addHandler(out_hdlr)
 log.setLevel(logging.INFO)
 
 service_list= [
-    # {"name": "gait", "url": os.environ.get('GAIT_URL')},
-    # {"name": "fms", "url": os.environ.get('FMS_URL')},
-    # {"name": "crt", "url": os.environ.get('CRT_URL')}
-        {"name": "gait", "url": "http://ga-app-service:3000"},
-        {"name": "fms", "url": "http://fms:3000"},
-        {"name": "crt", "url": "http://crt-acp.notprod.dq.homeoffice.gov.uk"}
+    {"name": "gait", "url": os.environ.get('GAIT_URL')},
+    {"name": "fms", "url": os.environ.get('FMS_URL')},
+    {"name": "crt", "url": os.environ.get('CRT_URL')}
+        # {"name": "gait", "url": "http://ga-app-service:3000"},
+        # {"name": "fms", "url": "http://fms:3000"},
+        # {"name": "crt", "url": "http://crt-acp.notprod.dq.homeoffice.gov.uk"}
     ]
 
 dic_list = []
