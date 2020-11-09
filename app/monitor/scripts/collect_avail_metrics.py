7448615a4101fe02e5ce82be52d6e12c8eb594e1
Haitham Abou Houly <haithamabouhouly@hodqadms-MacBook-Pro.local>
2020-11-09 18:31:41 +0000

    adding DRT

--- app/monitor/scripts/collect_avail_metrics.py
+++ app/monitor/scripts/collect_avail_metrics.py
@@ -27,8 +27,11 @@ service_list= [
 lambda_func_list = [
     {"name": "drt_ath", "func_name": os.environ.get('DRT_ATH_GRP')},
     {"name": "drt_jsn", "func_name": os.environ.get('DRT_JSN_GRP')},
-    {"name": "drt_rds", "func_name": os.environ.get('DRT_RDS_GRP')}
-    # {"name": "bf_", "func_name": os.environ.get('DRT_GRP')},
+    {"name": "drt_rds", "func_name": os.environ.get('DRT_RDS_GRP')},
+    {"name": "bf_api_parsed", "func_name": os.environ.get('BF_API_PRS')},
+    {"name": "bf_api_raw", "func_name": os.environ.get('BF_API_RAW')},
+    {"name": "bf_sch", "func_name": os.environ.get('BF_SCH')}
+
     ]
 
 fms_cert = '/APP/fms-certs/fms_cert'
@@ -89,41 +92,42 @@ def obtain_lambda_avail(lambda_name,func_name):
     time1min = datetime.datetime.now() - datetime.timedelta(minutes=1)
     timenowconv = timenow.timestamp() * 1000.0
     time1minconv = time1min.timestamp() * 1000.0
-    # lambda_logs = boto3.client('logs', region_name='eu-west-2')
-    #
-    # filter = lambda_logs.filter_log_events(logGroupName='/aws/lambda/'+func_name,
-    #                                         filterPattern='ERROR', startTime=int(time1minconv),
-    #                                         endTime=int(timenowconv))
-    # message = filter['events']
-    # if message == []:
-    #     lambda_health = 0
-    # else:
-    #     lambda_health = 2
-    #
-    # lambda_item = {lambda_name+'_health': lambda_health}
-    # lambda_list.append(lambda_item)
-    # log.info("Obtained the Availability status of "+lambda_name)
+    lambda_logs = boto3.client('logs', region_name='eu-west-2')
+
+    filter = lambda_logs.filter_log_events(logGroupName='/aws/lambda/'+func_name,
+                                            filterPattern='ERROR', startTime=int(time1minconv),
+                                            endTime=int(timenowconv))
+    message = filter['events']
+    if message == []:
+        lambda_health = 0
+    else:
+        lambda_health = 2
+
+    lambda_item = {lambda_name+'_health': lambda_health}
+    lambda_list.append(lambda_item)
+    log.info("Obtained the Availability status of "+lambda_name)
 
 def lambda_avail_check():
-    # for lam in lambda_func_list:
-    #     obtain_lambda_avail(lam['name'],lam['func_name'])
-    #
-    # drt_jsn_health = list(map(itemgetter('drt_jsn_health'), lambda_list))
-    # drt_ath_health = list(map(itemgetter('drt_ath_health'), lambda_list))
-    # drt_rds_health = list(map(itemgetter('drt_rds_health'), lambda_list))
-    #
-    # if (drt_jsn_health[0] == 0 and drt_rds_health[0] == 0 and drt_ath_health[0] == 0):
-    #     drt_status = 0
-    # elif ((bool(drt_jsn_health[0] == 0) ^ bool(drt_rds_health[0] == 0)) ^ bool(drt_ath_health[0] == 0)):
-    #     drt_status = 1
-    # else:
-    #     drt_status = 2
+    for lam in lambda_func_list:
+        obtain_lambda_avail(lam['name'],lam['func_name'])
+
+    drt_jsn_health = list(map(itemgetter('drt_jsn_health'), lambda_list))
+    drt_ath_health = list(map(itemgetter('drt_ath_health'), lambda_list))
+    drt_rds_health = list(map(itemgetter('drt_rds_health'), lambda_list))
+
+    if (drt_jsn_health[0] == 0 and drt_rds_health[0] == 0 and drt_ath_health[0] == 0):
+        drt_status = 0
+    elif ((bool(drt_jsn_health[0] == 0) ^ bool(drt_rds_health[0] == 0)) ^ bool(drt_ath_health[0] == 0)):
+        drt_status = 1
+    else:
+        drt_status = 2
 
     drt_status = 0
-    bf_status = 0
 
     dic_item = { 'name': "drt" , 'status': drt_status}
     dic_list.append(dic_item)
+
+    bf_status = 0
     dic_item = { 'name': "bfdp" , 'status': bf_status}
     dic_list.append(dic_item)
     # log.info("Obtained the Availability status of DRT")
