a73954d4676bdb22692dbcb78dba764831125302
Haitham Abou Houly <haithamabouhouly@hodqadms-MacBook-Pro.local>
2020-11-22 22:42:29 +0000

    add gait freshness

--- app/monitor/scripts/collect_freshness_metrics.py
+++ app/monitor/scripts/collect_freshness_metrics.py
@@ -3,6 +3,7 @@ import os,sys,subprocess
 import logging
 import boto3
 import psycopg2
+import time
 # from botocore.config import Config
 #
 # CONFIG = Config(
@@ -20,6 +21,7 @@ log.addHandler(out_hdlr)
 log.setLevel(logging.INFO)
 
 fresh_dic_list = []
+# temp_fms_list = []
 dic_item  = {}
 
 def get_ssm_parameters(param_name_list):
@@ -49,7 +51,8 @@ def get_ssm_parameters(param_name_list):
 
 def obtain_fms_fresh():
     """
-
+    Query the fms RDS DB for new rows
+    in the fms table
     """
     try:
         values = get_ssm_parameters(
@@ -66,22 +69,38 @@ def obtain_fms_fresh():
             'options': '-c statement_timeout=60000'
         }
 
-        dbstatement = "SELECT DISTINCT file_name from dq_fms.stg_tbl_api"
+        dbstatement_vio = """SELECT dv_load_dt FROM dq_fms.tbl_aviation_violations_status_by_schedule_stage WHERE "dv_load_dt" >= NOW() - INTERVAL '5 minutes' order by dv_load_dt desc"""
+        dbstatement_con = """SELECT cs_lastupdated FROM dq_fms.tbl_consolidated_schedule WHERE "cs_lastupdated" >= NOW() - INTERVAL '5 minutes' order by cs_lastupdated desc"""
+
+        #dbstatement = "SELECT DISTINCT file_name from dq_fms.stg_tbl_api"
 
         conn = psycopg2.connect(**conn_parameters)
         log.info('Connected to fms RDS')
 
         dbcur = conn.cursor()
 
-        fms_query = dbcur.execute(dbstatement)
-        fms_rows = dbcur.fetchall()
+        fms_vio_query = dbcur.execute(dbstatement_vio)
+        fms_vio_rows = dbcur.fetchall()
+        fms_con_query = dbcur.execute(dbstatement_con)
+        fms_con_rows = dbcur.fetchall()
 
-        if fms_rows == []:
-            fms_fresh_status = 2
+        if fms_vio_rows == []:
+            fms_vio_status = 2
         else:
-            fms_fresh_status = 0
+            fms_vio_status = 0
 
-        dic_item = { 'name': "fms_data" , 'status': fms_fresh_status}
+        if fms_con_rows == []:
+            fms_con_status = 2
+        else:
+            fms_con_status = 0
+
+        if (fms_vio_status == 0 and fms_con_status == 0):
+            status = 0
+        elif (bool(fms_vio_status == 0) ^ bool(fms_con_status == 0)):
+            status = 1
+        else:
+            status = 2
+        dic_item = { 'name': "fms_data" , 'status': status}
         fresh_dic_list.append(dic_item)
         log.info('Obtained the Freshness status of FMS data')
 
@@ -91,16 +110,92 @@ def obtain_fms_fresh():
         fresh_dic_list.append(dic_item)
         print (err)
 
+# def final_fms_check():
+#     temp_fms_list.clear()
+#     dic_item = {}
+#     fms_error=0
+#     for i in range(3):
+#         obtain_fms_fresh()
+#         time.sleep(120)
+#     for j in temp_fms_list:
+#         if j['status'] == 2:
+#             fms_error++1
+#     if fms_error == 3:
+#         dic_item = { 'name': "fms_data" , 'status': 2}
+#     else:
+#         dic_item = { 'name': "fms_data" , 'status': 0}
+#     fresh_dic_list.append(dic_item)
+
+def obtain_gait_fresh():
+    """
+    Query the GAIT RDS DB for new rows
+
+    """
+    try:
+        values = get_ssm_parameters(
+            ['/rds_gait_username',
+             '/rds_gait_password'])
+
+        conn_parameters = {
+            'host': '127.0.0.1',
+            'port': '8082',
+            'dbname': 'dqgaitrds2',
+            'user': values['user'],
+            'password': values['pass'],
+            'sslmode': 'require',
+            'options': '-c statement_timeout=60000'
+        }
+
+        dbstatement_fpl = """SELECT created_at FROM gama_fpl WHERE "created_at" >= NOW() - INTERVAL '5 minutes' order by created_at desc"""
+        dbstatement_gar = """SELECT created_at FROM gama_gar WHERE "created_at" >= NOW() - INTERVAL '5 minutes' order by created_at desc"""
+
+        conn = psycopg2.connect(**conn_parameters)
+        log.info('Connected to fms RDS')
+
+        dbcur = conn.cursor()
+
+        fpl_query = dbcur.execute(dbstatement_fpl)
+        fpl_rows = dbcur.fetchall()
+        gar_query = dbcur.execute(dbstatement_gar)
+        gar_rows = dbcur.fetchall()
+
+        if fpl_rows == []:
+            fpl_fresh_status = 2
+        else:
+            fpl_fresh_status = 0
+
+        if gar_rows == []:
+            gar_fresh_status = 2
+        else:
+            gar_fresh_status = 0
+
+        if (fpl_fresh_status == 0 and gar_fresh_status == 0):
+            status = 0
+        elif (bool(fpl_fresh_status == 0) ^ bool(gar_fresh_status == 0)):
+            status = 1
+        else:
+            status = 2
+        dic_item = { 'name': "gait_data" , 'status': status}
+        fresh_dic_list.append(dic_item)
+        log.info('Obtained the Freshness status of GAIT data')
+
+    except Exception as err:
+        # log.error("there is an error connecting to fms db: ",err)
+        dic_item = { 'name': "gait_data" , 'status': 2}
+        fresh_dic_list.append(dic_item)
+        print (err)
 
 def service_status_list():
     """
     create a list of services and the 2 or 0 code
     """
 
+    fresh_dic_list = []
+    dic_item  = {}
+
     log.info("Starting to fetch the freshness of each service....")
     fresh_dic_list.clear()
-
     obtain_fms_fresh()
-    # schedule.every(2).minutes.at(":00").do(obtain_fms_fresh)
+    obtain_gait_fresh()
 
     return fresh_dic_list
--- app/monitor/scripts/monitor_wrapper.py
+++ app/monitor/scripts/monitor_wrapper.py
@@ -13,6 +13,11 @@ out_hdlr.setLevel(logging.INFO)
 log.addHandler(out_hdlr)
 log.setLevel(logging.INFO)
 
+fresh_list = []
+
+def retreive_fresh():
+    fresh_list = fresh()
+
 def write_to_json():
     """
     create the tarcing.json file the will be grabed by the
@@ -20,13 +25,13 @@ def write_to_json():
     """
     try:
         f = open("/APP/scripts/tracing.json", "w")
-        print("a.avail_dic_list is: ",avail())
+        print("avail_dic_list is: ",avail())
         for item in avail():
             f.write("# HELP availability_of_"+item['name']+ " to check service availability \n")
             f.write("dq_"+item['name']+"_availability " +str(item['status'])+ "\n")
 
-        print("f.fresh_dic_list is: ",fresh())
-        for item in fresh():
+        print("fresh_dic_list is: ",fresh_list)
+        for item in fresh_list:
             f.write("# HELP freshness_of_"+item['name']+ " to check data freshness \n")
             f.write("dq_"+item['name']+"_freshness " +str(item['status'])+ "\n")
         f.close()
@@ -36,8 +41,8 @@ def write_to_json():
 
 def main():
     log.info("Starting Scheduler......")
-    # schedule.every(1).minutes.at(":00").do(service_status_list)
-    schedule.every(2).minutes.at(":00").do(write_to_json)
+    schedule.every(5).minutes.at(":00").do(retreive_fresh)
+    schedule.every(1).minutes.at(":00").do(write_to_json)
     while True:
         schedule.run_pending()
         time.sleep(1)
