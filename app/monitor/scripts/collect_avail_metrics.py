d6eea7883f72f2b477da801198208a01593f67c1
Haitham Abou Houly <haithamabouhouly@hodqadms-MacBook-Pro.local>
2020-12-04 01:33:48 +0000

    tweek avail

--- app/monitor/scripts/collect_avail_metrics.py
+++ app/monitor/scripts/collect_avail_metrics.py
@@ -43,7 +43,10 @@ lambda_func_list = [
     {"name": "drt_rds", "func_name": os.environ.get('DRT_RDS_GRP')},
     {"name": "bf_api_parsed", "func_name": os.environ.get('BF_API_PRS')},
     {"name": "bf_api_raw", "func_name": os.environ.get('BF_API_RAW')},
-    {"name": "bf_sch", "func_name": os.environ.get('BF_SCH')},
+    {"name": "bf_sch_cns", "func_name": os.environ.get('BF_SCH_CNS')},
+    {"name": "bf_sch_acl", "func_name": os.environ.get('BF_SCH_ACL')},
+    {"name": "bf_sch_fs", "func_name": os.environ.get('BF_SCH_FS')},
+    {"name": "bf_sch_oag", "func_name": os.environ.get('BF_SCH_OAG')},
     {"name": "bf_xrs_ath", "func_name": os.environ.get('BF_XRS_ATH')},
     {"name": "bf_rls_ath", "func_name": os.environ.get('BF_RLS_ATH')},
     {"name": "bf_asr_ath", "func_name": os.environ.get('BF_ASR_ATH')},
@@ -55,6 +58,7 @@ fms_key = '/APP/auth-files/fms_key'
 avail_dic_list = []
 avail_api_pod_list = []
 lambda_list = []
+lam_list = []
 dic_item  = {}
 lambda_item = {}
 
@@ -125,9 +129,38 @@ def obtain_lambda_avail(lambda_name,func_name):
     time1minconv = time1min.timestamp() * 1000.0
     lambda_logs = boto3.client('logs',  region_name="eu-west-2")
 
-    filter = lambda_logs.filter_log_events(logGroupName='/aws/lambda/'+func_name,
+    paginator = lambda_logs.get_paginator('filter_log_events')
+    filter_1 = paginator.paginate(logGroupName='/aws/lambda/'+func_name,
                                             filterPattern='ERROR', startTime=int(time1minconv),
                                             endTime=int(timenowconv))
+    filter_2 = paginator.paginate(logGroupName='/aws/lambda/'+func_name,
+                                            filterPattern='Fail', startTime=int(time1minconv),
+                                            endTime=int(timenowconv))
+    filter_3 = paginator.paginate(logGroupName='/aws/lambda/'+func_name,
+                                            filterPattern='fail', startTime=int(time1minconv),
+                                            endTime=int(timenowconv))
+
+    for page in filter_1:
+        for i in page['events']:
+            lam_list.append(i['message'])
+    for page in filter_2:
+        for i in page['events']:
+            lam_list.append(i['message'])
+    for page in filter_3:
+        for i in page['events']:
+            lam_list.append(i['message'])
+
+    if rds_list == []:
+        drt_rds_status = 2
+    else:
+        drt_rds_status = 0
+
+    # filter_1 = lambda_logs.filter_log_events(logGroupName='/aws/lambda/'+func_name,
+    #                                         filterPattern='ERROR', startTime=int(time1minconv),
+    #                                         endTime=int(timenowconv))
+    # filter_2 = lambda_logs.filter_log_events(logGroupName='/aws/lambda/'+func_name,
+    #                                         filterPattern='Fail', startTime=int(time1minconv),
+    #                                         endTime=int(timenowconv))
     message = filter['events']
     if message == []:
         lambda_health = 0
@@ -145,35 +178,41 @@ def lambda_avail_check():
 
     for lam in lambda_list:
         if lam['name'] == 'drt_ath':
-            drt_ath_health = lam['status']
+            drt_ath = lam['status']
         if lam['name'] == 'drt_jsn':
-            drt_jsn_health = lam['status']
+            drt_jsn = lam['status']
         if lam['name'] == 'drt_rds':
-            drt_rds_health = lam['status']
+            drt_rds = lam['status']
         if lam['name'] == 'bf_api_parsed':
-            bf_api_parsed_health = lam['status']
+            bf_api_parsed = lam['status']
         if lam['name'] == 'bf_api_raw':
-            bf_api_raw_health = lam['status']
-        if lam['name'] == 'bf_sch':
-            bf_sch_health = lam['status']
+            bf_api_raw = lam['status']
+        if lam['name'] == 'bf_sch_cns':
+            bf_sch_cns = lam['status']
+        if lam['name'] == 'bf_sch_acl':
+            bf_sch_acl = lam['status']
+        if lam['name'] == 'bf_sch_fs':
+            bf_sch_fs = lam['status']
+        if lam['name'] == 'bf_sch_oag':
+            bf_sch_oag = lam['status']
         if lam['name'] == 'bf_xrs_ath':
-            bf_xrs_ath_health = lam['status']
+            bf_xrs_ath = lam['status']
         if lam['name'] == 'bf_rls_ath':
-            bf_rls_ath_health = lam['status']
+            bf_rls_ath = lam['status']
         if lam['name'] == 'bf_asr_ath':
-            bf_asr_ath_health = lam['status']
+            bf_asr_ath = lam['status']
         if lam['name'] == 'bf_as_ath':
-            bf_as_ath_health = lam['status']
+            bf_as_ath = lam['status']
 
-    if drt_jsn_health == 0 and drt_rds_health == 0 and drt_ath_health == 0:
+    if drt_jsn == 0 and drt_rds == 0 and drt_ath == 0:
         drt_status = 0
-    if drt_jsn_health != 0:
+    if drt_jsn != 0:
         drt_status = 1
-    if drt_rds_health != 0:
+    if drt_rds != 0:
         drt_status = 1
-    if drt_ath_health != 0:
+    if drt_ath != 0:
         drt_status = 1
-    if drt_jsn_health != 0 and drt_rds_health != 0 and drt_ath_health != 0:
+    if drt_jsn != 0 and drt_rds != 0 and drt_ath != 0:
         drt_status = 2
 
     dic_item = { 'name': "drt" , 'status': drt_status}
@@ -185,26 +224,26 @@ def lambda_avail_check():
     obtain_api_pod_avail()
     for pod in avail_api_pod_list:
         if pod['name'] == 'gait_api':
-            api_gait_health = pod['status']
+            api_gait = pod['status']
         if pod['name'] == 'api-msk':
-            api_msk_health = pod['status']
+            api_msk = pod['status']
         if pod['name'] == 'gait_sgar':
-            api_sgar_health = pod['status']
+            api_sgar = pod['status']
 
     ## Obtain api lambdas avial
-    if bf_api_parsed_health == 0 and bf_api_raw_health == 0 and api_gait_health == 0 and api_msk_health == 0 and api_sgar_health == 0:
+    if bf_api_parsed == 0 and bf_api_raw == 0 and api_gait == 0 and api_msk == 0 and api_sgar == 0:
         bf_api_status = 0
-    if bf_api_parsed_health != 0:
+    if bf_api_parsed != 0:
         bf_api_status = 1
-    if bf_api_raw_health != 0:
+    if bf_api_raw != 0:
         bf_api_status = 1
-    if api_gait_health != 0:
+    if api_gait != 0:
         bf_api_status = 1
-    if api_msk_health != 0:
+    if api_msk != 0:
         bf_api_status = 1
-    if api_sgar_health != 0:
+    if api_sgar != 0:
         bf_api_status = 1
-    if bf_api_parsed_health != 0 and bf_api_raw_health != 0 and api_gait_health != 0 and api_msk_health != 0 and api_sgar_health != 0:
+    if bf_api_parsed != 0 and bf_api_raw != 0 and api_gait != 0 and api_msk != 0 and api_sgar != 0:
         bf_api_status = 2
 
     dic_item = { 'name': "bf_api" , 'status': bf_api_status}
@@ -212,17 +251,17 @@ def lambda_avail_check():
     log.info("Obtained the Availability status of BFDP API")
 
     # bf scoring  files
-    if (bf_xrs_ath_health == 0 and bf_rls_ath_health == 0 and bf_asr_ath_health == 0 and bf_as_ath_health == 0):
+    if (bf_xrs_ath == 0 and bf_rls_ath == 0 and bf_asr_ath == 0 and bf_as_ath == 0):
         bf_scr_status = 0
-    if bf_xrs_ath_health != 0:
+    if bf_xrs_ath != 0:
         bf_scr_status = 1
-    if bf_rls_ath_health != 0:
+    if bf_rls_ath != 0:
         bf_scr_status = 1
-    if bf_asr_ath_health != 0:
+    if bf_asr_ath != 0:
         bf_scr_status = 1
-    if bf_as_ath_health != 0:
+    if bf_as_ath != 0:
         bf_scr_status = 1
-    if bf_xrs_ath_health != 0 and bf_rls_ath_health != 0 and bf_asr_ath_health != 0 and bf_as_ath_health != 0:
+    if bf_xrs_ath != 0 and bf_rls_ath != 0 and bf_asr_ath != 0 and bf_as_ath != 0:
         bf_scr_status = 2
 
     dic_item = { 'name': "bf_scr" , 'status': bf_scr_status}
@@ -230,14 +269,20 @@ def lambda_avail_check():
     log.info("Obtained the Availability status of BFDP SCR")
 
     #  bf Scheduling state
-    # if (bf_api_status == 0 and bf_sch_health == 0 and bf_scr_status == 0):
-    #     bf_status = 0
-    # elif ((bool(bf_api_status == 0) ^ bool(bf_sch_health == 0)) ^ bool(bf_scr_status == 0)):
-    #     bf_status = 1
-    # else:
-    #     bf_status = 2
+    if (bf_sch_fs == 0 and bf_sch_cns == 0 and bf_sch_acl == 0 and bf_sch_oag == 0):
+        bf_sch_status = 0
+    if bf_sch_fs != 0:
+        bf_sch_status = 1
+    if bf_sch_cns != 0:
+        bf_sch_status = 1
+    if bf_sch_acl != 0:
+        bf_sch_status = 1
+    if bf_sch_oag != 0:
+        bf_sch_status = 1
+    if bf_sch_fs != 0 and bf_sch_cns != 0 and bf_sch_acl != 0 and bf_sch_oag != 0:
+        bf_sch_status = 2
 
-    dic_item = { 'name': "bf_sch" , 'status': bf_sch_health}
+    dic_item = { 'name': "bf_sch" , 'status': bf_sch_status}
     avail_dic_list.append(dic_item)
     log.info("Obtained the Availability status of BFDP SCH")
 
