118120d52c6d923e9d4d7bdc8dba94565b4dfd20
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2021-09-03 13:13:03 +0100

    add slack alerts to frontebd avil

--- app/monitor/scripts/collect_avail_metrics.py
+++ app/monitor/scripts/collect_avail_metrics.py
@@ -72,12 +72,9 @@ def alert_to_slack(service, status_code, check_type):
         if check_type is 'fresh':
             title = ":fire: :sad_parrot: There seems to be an issue with" +service+ "Data Freshness :sad_parrot: :fire:"
         slack_data = {
-            "username": service + "Bot",
-            "icon_emoji": ":warning:",
-            "text": title,
+            "text": title.format(text),
             "attachments": [
                 {
-                    "text": "{0}".format(text),
                     "color": "#EE3333",
                     "attachment_type": "default",
                     "fields": [
