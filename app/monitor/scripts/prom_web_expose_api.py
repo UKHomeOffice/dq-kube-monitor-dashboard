d94d9ae631db11e931a828c26c5bf46c19b66ec2
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2022-07-15 14:30:42 +0100

    add no zips script

--- app/monitor/scripts/prom_web_expose_api.py
+++ app/monitor/scripts/prom_web_expose_api.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 class StoreHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/dq-monitor/prometheus':
+        if self.path == '/dq-monitor/api':
             with open('/APP/scripts/api.json') as fh:
                 self.send_response(200)
                 self.send_header('Content-type', 'text/json')
